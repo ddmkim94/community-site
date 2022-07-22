15cfbc8fc6f2ce3e3330c7a459df59940d15c31d
ddmkim94 <ddmkim94@gmail.com>
2022-07-22 16:40:04 +0900

    회원 DTO 생성자 & MemberStatus Enum 추가

--- src/main/java/com/ll/cmu/dto/Member.java
+++ src/main/java/com/ll/cmu/dto/Member.java
@@ -8,8 +8,78 @@ public class Member {
     private LocalDateTime modifyDate;
     private String username;
     private String password;
+    private String name;
     private String email;
     private MemberStatus adminLevel;
 
+    public Member(String username, String password, String name, String email) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public LocalDateTime getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(LocalDateTime modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public MemberStatus getAdminLevel() {
+        return adminLevel;
+    }
+
+    public void setAdminLevel(MemberStatus adminLevel) {
+        this.adminLevel = adminLevel;
+    }
 }
--- src/main/java/com/ll/cmu/dto/MemberStatus.java
+++ src/main/java/com/ll/cmu/dto/MemberStatus.java
@@ -1,5 +1,15 @@
 package com.ll.cmu.dto;
 
 public enum MemberStatus {
-    USER, ADMIN
+    USER("3"), ADMIN("6");
+
+    private final String level;
+
+    MemberStatus(String level) {
+        this.level = level;
+    }
+
+    public String getLevel(){
+        return level;
+    }
 }
